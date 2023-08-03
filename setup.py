3aeee86a74fc09fbc9b661a1111d460a1af518b2
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2023-08-03 11:12:26 -0800

    v0.2.0 with most queries present

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="pyboko",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(include=["pyboko", "pyboko.*"]),
     license="GPLv3",
     install_requires=["aiohttp"],
